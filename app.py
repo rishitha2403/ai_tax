0492e826167bd42923b06b07b4a9c25ed527faa5
Swift <hellosai109@gmail.com>
2025-02-26 00:39:46 +0530

    Login added commit

--- app.py
+++ app.py
@@ -66,9 +66,13 @@ def result():
         return "No result found", 404
     return render_template("result.html", result=result)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("view.html")
+
+@app.route("/")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
